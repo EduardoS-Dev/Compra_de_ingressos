d0c98f4642aa26cc184c8733b276a3d9231f73e6
Eduardo Soares <eduardo.soaressilva@icloud.com>
2024-05-11 20:36:28 -0300

    Aceitando apenas quantidade válida para compra do ingresso

--- js/app.js
+++ js/app.js
@@ -8,24 +8,31 @@ function comprar(){
     let campoQtdePista = document.getElementById('qtd-pista');
     let campoQtdeSuperior = document.getElementById('qtd-superior');
     let campoQtdeInferior = document.getElementById('qtd-inferior');
-    
-    if(tipoIngresso == 'pista'){
-        comprarIngressos(tipoIngresso, qtdeIngresso, 'pista', campoQtdePista)
+
+    if(qtdeIngresso <= 0){
+        alert(`Informe uma quantidade válida.`)
     } else {
-        if(tipoIngresso == 'superior'){
-            comprarIngressos(tipoIngresso, qtdeIngresso, 'superior', campoQtdeSuperior)
+        if(tipoIngresso == 'pista'){
+            comprarIngressos(tipoIngresso, qtdeIngresso, 'pista', campoQtdePista)
         } else {
-            comprarIngressos(tipoIngresso, qtdeIngresso, 'inferior', campoQtdeInferior)
-        }
-    } 
+            if(tipoIngresso == 'superior'){
+                comprarIngressos(tipoIngresso, qtdeIngresso, 'superior', campoQtdeSuperior)
+            } else {
+                comprarIngressos(tipoIngresso, qtdeIngresso, 'inferior', campoQtdeInferior)
+            }
+        } 
+    }
+    
+    
        
 }
 
 function comprarIngressos(tipo, qtde, local , texto){
     if(tipo == local && (texto.textContent - qtde) >= 0){
+        alert('Compra realizada com sucesso!')
         texto.innerHTML = `<span id="qtd-${tipo}">${texto.textContent - qtde}</span>`
     } else {
-        let frase = texto.textContent > 1 ? `Restam ${texto.textContent} ingressos disponíveis` : `Apenas ${texto.textContent} disponível.`
+        let frase = texto.textContent > 0 ? `${texto.textContent} disponível` : `${tipo} [ESGOTADO]`
         alert(frase)
     }
 }
